bb18bcbf273cb7a4cfa192a5c25379e6fc7d7794
Cherrell Finister <cfiniste@lion.lmu.edu>
2019-03-26 02:27:41 -0700

    JS
    figuring out the server

---
+++ clock_app/mobileframe.js
@@ -0,0 +1,14 @@
+//This is the client application
+//hold the data and requests the calls to the server (Clock)
+
+//MUST be done in a websocket
+
+"use strict";
+
+(() => {
+  window.ServerController = {
+    init: () => {
+
+    }
+  }
+}
